f3bd02671de19a914fee58ea9e18838a436be80c
Maksim <mashnert@mail.ru>
2022-11-26 01:05:56 +0300

    initial commit

---
+++ src/main/java/com/georgia/me/springredisdemo/SpringRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.georgia.me.springredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisDemoApplication.class, args);
+    }
+
+}
